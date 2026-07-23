9a0c53093c59b8df1776986386b29d087c1d178b
uditpanthi <uditpanthi31@gmail.com>
2026-07-23 17:21:02 +0530

    swagger update to test in Azure

--- CozyHavenStayServer/CozyHavenStayServer/Program.cs
+++ CozyHavenStayServer/CozyHavenStayServer/Program.cs
@@ -193,11 +193,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 
 app.UseHttpsRedirection();
